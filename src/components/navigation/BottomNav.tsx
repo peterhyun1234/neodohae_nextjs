1b5de4b551da176c30a5a8ff0d6d982db65a00ab
전현빈[SW Developer] <peterhyun1234@gmail.com>
2023-07-19 23:44:10 +0900

    feat: change padding foe navbar

--- src/components/navigation/BottomNav.tsx
+++ src/components/navigation/BottomNav.tsx
@@ -1,6 +1,6 @@
 import { SetStateAction, useEffect, useState } from 'react';
-import Styled from 'styled-components';
-import { useRouter } from 'next/router';
+import Styled from 'styled-components'
+import { useRouter } from 'next/router'
 
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import { styled } from '@mui/material/styles';
@@ -11,53 +11,48 @@ import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
 import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 
+
 const CustomBottomNavigation = styled(BottomNavigation)({
-  '& .Mui-selected': {
-    color: '#5a5adc',
+  "& .Mui-selected": {
+    color: "#5a5adc",
   },
-  '& .MuiBottomNavigationAction-label': {
-    fontSize: '0.8rem',
-    marginTop: '4px',
+  "& .MuiBottomNavigationAction-label": {
+    fontSize: "0.8rem",
+    marginTop: "4px",
   },
-  '& .MuiButtonBase-root': {
-    minWidth: '70px',
+  "& .MuiButtonBase-root": {
+    minWidth: "70px",
   },
 });
 
 const BottomNav = () => {
-  const router = useRouter();
-  const [value, setValue] = useState(0);
+    const router = useRouter();
+    const [value, setValue] = useState(0);
 
-  const paths = ['/', '/todo', '/schedule', '/chat', '/myPage'];
+    const paths = ['/', '/todo', '/schedule', '/chat', '/myPage'];
 
-  useEffect(() => {
-    setValue(paths.indexOf(router.pathname));
-  }, [router]);
+    useEffect(() => {
+        setValue(paths.indexOf(router.pathname));
+    }, [router]);
 
-  return (
-    <WrapBox>
-      <CustomBottomNavigation
-        value={value}
-        onChange={(event: any, newValue: any) => {
-          setValue(newValue);
-          router.push(paths[newValue]);
-        }}
-        showLabels
-      >
-        <BottomNavigationAction label="홈" icon={<HomeRoundedIcon />} />
-        <BottomNavigationAction
-          label="To-Do"
-          icon={<FormatListNumberedRoundedIcon />}
-        />
-        <BottomNavigationAction
-          label="스케줄"
-          icon={<CalendarMonthRoundedIcon />}
-        />
-        <BottomNavigationAction label="채팅" icon={<ChatRoundedIcon />} />
-        <BottomNavigationAction label="마이" icon={<PersonRoundedIcon />} />
-      </CustomBottomNavigation>
-    </WrapBox>
-  );
+    return (
+        <WrapBox>
+            <CustomBottomNavigation
+                value={value}
+                onChange={(event: any, newValue: any) => {
+                    setValue(newValue);
+                    router.push(paths[newValue]);
+                }}
+                showLabels
+            >
+                <BottomNavigationAction label="홈" icon={<HomeRoundedIcon />} />
+                <BottomNavigationAction label="To-Do" icon={<FormatListNumberedRoundedIcon />} />
+                <BottomNavigationAction label="스케줄" icon={<CalendarMonthRoundedIcon />} />
+                <BottomNavigationAction label="채팅" icon={<ChatRoundedIcon />} />
+                <BottomNavigationAction label="마이" icon={<PersonRoundedIcon />} />
+            </CustomBottomNavigation>
+        </WrapBox>
+    );
 };
 
 const WrapBox = Styled.div`
@@ -75,8 +70,8 @@ const WrapBox = Styled.div`
       z-index: 5;
       background: #ffffff;
       padding-top: 10px;
-      padding-bottom: calc(10px + env(safe-area-inset-bottom));
+      padding-bottom: 10px;
     }
-`;
+`
 
 export default BottomNav;
